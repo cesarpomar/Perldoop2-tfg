b8d1722e9347212a032d6bd45a18160f4389c37f
Cesar Pomar <Cesar Pomar>
2016-07-07 02:39:18 +0200

    Versión Entregada para presentación del proyecto, añadida licencia.

--- perldoop2/libs/casting.py
+++ perldoop2/libs/casting.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#Copyright 2016 César Pomar <cesarpomar18@gmail.com>
+#
+#This file is part of Perldoop.
+#
+#Perldoop is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Perldoop is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Perldoop.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 from libs import Messages as Msg
 from libs import DataType as Dtp
